8cb92848fda896bfe71e1df75f88d207f6538092
minjung <minjony1014@gmail.com>
2019-08-10 02:36:18 +0900

    favorite add

--- fortune/views.py
+++ fortune/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import Fortune,User
+from .models import Fortune, User
 from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
 
+
 def lottery(request):
     fortune = Fortune.objects
-    return render(request, 'lottery.html',{'fortune': fortune})
+    return render(request, 'lottery.html', {'fortune': fortune})
+
 
 def enroll(request):
     email = request.GET.get('email')
-    user = User(fortune_id= 1, email = email)
+    user = User(fortune_id=1, email=email)
     user.save()
 
     return redirect('lottery')
+
 
 def sendEmail(request):
 
@@ -26,21 +29,21 @@ def sendEmail(request):
     print(fortune.seat)
 
     send_mail(
-    '🥠포춘 티켓 응모 결과',
-    msg_plain,
-    settings.EMAIL_HOST_USER,
-    ['starpin1014@likelion.org'],
-    html_message=msg_html,
-    fail_silently=False
-    ) 
+        '🥠포춘 티켓 응모 결과',
+        msg_plain,
+        settings.EMAIL_HOST_USER,
+        ['mnjony1014@gmail.com'],
+        html_message=msg_html,
+        fail_silently=False
+    )
 
     send_mail(
         '🥠포춘 티켓 응모 결과',
         msg_plain,
         settings.EMAIL_HOST_USER,
-        ['starpin1014@likelion.org'],
+        ['jmtkdsh@gmail.com'],
         html_message=msg_html,
         fail_silently=False
-        ) 
+    )
 
     return redirect('lottery')
--- theme/views.py
+++ theme/views.py
@@ -3,25 +3,6 @@ from django.views.generic. base import TemplateView
 from show.models import Show
 
 # Create your views here.
-<<<<<<< HEAD
 def MainpageView(request):
     show = Show.objects.all()
     return render(request,'index.html',{'show':show})
-
-msg_plain = render_to_string('email.txt')
-msg_html = render_to_string('email.html')
-
-''' send_mail(
-    '🥠포춘 티켓 응모 결과',
-    msg_plain,
-    settings.EMAIL_HOST_USER,
-    ['starpin1014@likelion.org'],
-    html_message=msg_html,
-    fail_silently=False
-) '''
-=======
-
-def MainpageView(request):
-    show = Show.objects.all()
-    return render(request,'index.html',{'show':show})
->>>>>>> 1230a10ab1f50034f70a0d86a306252d552edd35
