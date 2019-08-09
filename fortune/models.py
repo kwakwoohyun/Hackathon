6bfae80f930c28fe5733c4cc1f02d0a0813fa24b
minjung <minjony1014@gmail.com>
2019-08-09 17:39:57 +0900

    re

--- fortune/models.py
+++ fortune/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Fortune(models.Model):
+
+    show_id = models.IntegerField()
+    date = models.CharField(max_length=500)
+    price = models.CharField(max_length=500)
+    starttime = models.CharField(max_length=500)
+    finishtime = models.CharField(max_length=500)
+    number = models.IntegerField()
+    calcuatetime = models.CharField(max_length=500)
+
+class User(models.Model):
+    fortune_id = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user_id
--- fortune/views.py
+++ fortune/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Fortune,User
 
-# Create your views here.
+def lottery(request):
+    fortune = Fortune.objects
+    return render(request, 'lottery.html',{'fortune': fortune})
+
+def enroll(request):
+    
+    user = User(fortune_id= 1, email = 'starpin1014@likelion.org')
+    user.save()
+
+    return redirect('lottery')
